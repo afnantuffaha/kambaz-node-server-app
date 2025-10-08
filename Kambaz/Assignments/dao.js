7204eea6b8dcdc56fad13968d4c368596140da54
Afnan Tuffaha <145280603+afnantuffaha@users.noreply.github.com>
2025-10-08 14:49:34 -0400

    update dao

--- Kambaz/Assignments/dao.js
+++ Kambaz/Assignments/dao.js
@@ -1,34 +1,51 @@
 import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 
-export function findAssignmentsForCourse(courseId) {
-  const { assignments } = Database;
-  return assignments.filter((assignment) => assignment.course === courseId);
-}
+// export function findAssignmentsForCourse(courseId) {
+//   const { assignments } = Database;
+//   return assignments.filter((assignment) => assignment.course === courseId);
+// }
 
-export function createAssignment(assignment) {
-  const newAssignment = { ...assignment, _id: uuidv4() };
-  Database.assignments = [...Database.assignments, newAssignment];
-  return newAssignment;
-}
+// export function createAssignment(assignment) {
+//   const newAssignment = { ...assignment, _id: uuidv4() };
+//   Database.assignments = [...Database.assignments, newAssignment];
+//   return newAssignment;
+// }
 
-export function deleteAssignment(assignmentId) {
-  const { assignments } = Database;
-  Database.assignments = assignments.filter((assignment) => assignment._id !== assignmentId);
-  return { success: true };
-}
+// export function deleteAssignment(assignmentId) {
+//   const { assignments } = Database;
+//   Database.assignments = assignments.filter((assignment) => assignment._id !== assignmentId);
+//   return { success: true };
+// }
 
-export function updateAssignment(assignmentId, assignmentUpdates) {
-  const { assignments } = Database;
-  const assignment = assignments.find((assignment) => assignment._id === assignmentId);
-  if (assignment) {
-    Object.assign(assignment, assignmentUpdates);
-    return assignment;
-  }
-  return null;
-}
+// export function updateAssignment(assignmentId, assignmentUpdates) {
+//   const { assignments } = Database;
+//   const assignment = assignments.find((assignment) => assignment._id === assignmentId);
+//   if (assignment) {
+//     Object.assign(assignment, assignmentUpdates);
+//     return assignment;
+//   }
+//   return null;
+// }
 
 export function findAssignmentById(assignmentId) {
   const { assignments } = Database;
   return assignments.find((assignment) => assignment._id === assignmentId);
+}
+
+export function findAssignmentsForCourse(courseId) {
+    return model.find({ course: courseId })
+}
+
+export function createAssignment(assignment) {
+    const newAssignment = { ...assignment, _id: uuidv4() };
+    return model.create(newAssignment);
+}
+
+export function deleteAssignment(assignmentId) {
+    return model.deleteOne({ _id: assignmentId })
+}
+
+export function updateAssignment(assignmentId, assignmentUpdates) {
+    return model.updateOne({ _id: assignmentId}, assignmentUpdates)
 }
